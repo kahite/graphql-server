4cb7656441cd42fbf2bbfe674fcfed32fa9b9734
kahite <lelievre.yves@gmail.com>
2019-07-16 10:04:18 +0200

    Simple user CRUD

--- src/resolvers.js
+++ src/resolvers.js
@@ -2,7 +2,34 @@ import {users} from "./db";
 
 const resolvers = {
 	Query: {
-		hello: () => "Hello World!"
+		users: (parent, args, context, info) => {
+			return users;
+		},
+		user: (parent, { id }, context, info) => {
+			return users.find(user => user.id == id)
+		}
+	},
+	Mutation: {
+		createUser: (parent, { id, name, email, age }, context, info) => {
+			const newUser = { id, name, email, age};
+			users.push(newUser);
+
+			return newUser;
+		},
+		updateUser: (parent, { id, name, email, age }, context, info) => {
+			let user = users.find(user => user.id == id);
+			user.name = name;
+			user.email = email;
+			user.age = age;
+			
+			return user;
+		},
+		deleteUser: (parent, { id }, context, info) => {
+			let userIndex = users.findIndex(user => user.id == id);
+			const deletedUser = users.splice(userIndex, 1);
+
+			return deletedUser[0];
+		}
 	}
 };
 
